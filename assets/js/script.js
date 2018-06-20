60c19f86329c7acbe3263b9f97d0ee1e633d23d4
Rahib <rahib.rustemzade@gmail.com>
2018-06-20 18:35:26 +0400

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,46 @@ $(document).ready(function() {
         dots: false,
         infinite: true,
         slidesToShow: 5,
+        slidesToScroll: 5,
+        arrows: true,
+        speed: 800,
+        autoplay:false,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 777,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          
+        ]
+      
+    
+      
+      })//slider 1 
+
+
+      $(".regular2").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 5,
         slidesToScroll: 1,
         arrows: false,
         autoplay:true,
@@ -40,8 +80,8 @@ $(document).ready(function() {
       });
     
       
-      })//slider
-
+      })//slider 2
+      
 
 
 
